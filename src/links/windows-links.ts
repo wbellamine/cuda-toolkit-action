a95c2d022488d00a1d5c9dbe90b0a02200ec44ed
Wadii Bellamine <100240674+wbellamine@users.noreply.github.com>
2022-12-29 16:51:27 -0500

    Added link for Cuda 12

--- src/links/windows-links.ts
+++ src/links/windows-links.ts
@@ -25,6 +25,10 @@ export class WindowsLinks extends AbstractLinks {
 
   private cudaVersionToNetworkUrl: Map<string, string> = new Map([
     [
+      '12.0.0',
+      'https://developer.download.nvidia.com/compute/cuda/12.0.0/network_installers/cuda_12.0.0_windows_network.exe'
+    ],
+    [
       '11.7.0',
       'https://developer.download.nvidia.com/compute/cuda/11.7.0/network_installers/cuda_11.7.0_windows_network.exe'
     ],
